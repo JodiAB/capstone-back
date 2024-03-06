f2dfafb94742ef773921ffeee5b07fef46ac5c71
JodiAB <jodiabrahams9@gmail.com>
2024-03-06 15:51:05 +0200

    4 commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import {config} from 'dotenv';
 import cors from 'cors';
-import router from '../backend/routes/items.js'
+import router from './routes/items.js'
 import cookieParser from 'cookie-parser';
 
 
