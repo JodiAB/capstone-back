fa14c0fd321785148c41a949b5c7584c3a7c6811
JodiAB <jodiabrahams9@gmail.com>
2024-03-16 10:15:14 +0200

    meToo

--- routes/user.js
+++ routes/user.js
@@ -2,15 +2,18 @@ import express from "express";
 import controller from "../controller/user.js";
 const userRouter = express.Router();
 
-userRouter
-    .route('/')
-        .get(controller.getUsers)
-        .post(controller.postUser);
 
-userRouter
-    .route("/:id")
-        .get(controller.getUserById) 
-        .delete(controller.deletePerson)
-        .patch(controller.patchUser);
+userRouter.route('/register')
+    .post(controller.postUser); 
+
+
+userRouter.route('/users')
+    .get(controller.getUsers); 
+
+
+userRouter.route("/:id")
+    .get(controller.getUserById) 
+    .delete(controller.deletePerson)
+    .patch(controller.patchUser);
 
 export default userRouter;
